8733ff7094061017f3fd68bb678273f829527cb5
HadesSuhen <jjx_wxt@163.com>
2014-09-29 11:30:08 +0800

    2.0

--- SwiftApp_2/ViewController.swift
+++ SwiftApp_2/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
                             
+    @IBOutlet var textField1 : UITextField
+    @IBOutlet var textField2 : UITextField
+    @IBOutlet var buttonAdd : UIButton
+    @IBOutlet var labelResult : UILabel
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func didClicked(sender : UIButton) {
+        var result = 0
+        var value1 = self.textField1.text.toInt()  //Optional类型  不一定可以转换成功
+        var value2 = self.textField2.text.toInt()
+        result = value1! + value2!  //使用Optional类型  后面要加！
+        println("result = \(result)")
+        
+        self.labelResult.text = "\(result)"
+        
+    }
 
 }
 
